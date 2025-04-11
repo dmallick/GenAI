8506e96dbf9093e9d4f6dbedfa4fa06952a415d0
Debasish Mallick <debasish.mallick@gmail.com>
2025-04-11 09:12:32 +0530

    init: .vscode/settings.json

---
+++ lanfchaintest.py
@@ -0,0 +1,8 @@
+import pytest
+# Assuming your langchain.py has some functions to test
+# For example, if langchain.py has a function called process_text
+# from your_package import langchain # If langchain.py is part of a package
+import langchain # if langchain.py is in the same directory
+
+def test_hello_world():
+    assert langchain.hello() == "hello"
